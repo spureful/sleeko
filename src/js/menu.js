d4d8bf7785107de5d1c1d5c9b630b2c9d7087b28
Ann <rw1zz@yandex.ru>
2019-09-23 15:51:41 +0300

    add script for mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,41 @@
 (function mobileMenu() {
+        const promoSection = document.querySelector('.promo');
+        const menuModal = promoSection.firstElementChild;
+        const menuList = menuModal.firstElementChild;
+        const menuItems = menuList.querySelectorAll('.nav__item');
+        const promoBtn = document.querySelector('.promo__btn');
+        const menuBtn = document.querySelector('.nav-btn');
 
-    const menuBtn = document.querySelector('.nav-btn');
-    const menuModal = document.querySelector('.nav');
-    const menuItem = document.querySelectorAll('.nav__item');
-    
-    console.log(menuBtn);
-    
-    menuBtn.addEventListener('click', function() {
-        console.log('btn');
-        menuModal.classList.add('active');
-        console.log('btgn');
-    });
+   
+      
+        menuBtn.addEventListener('click', function () {
+
+            if (menuModal.classList.contains('active')) {
+                menuModal.classList.remove('active');
+                menuBtn.classList.remove('nav-btn-active');
+            } else {
+                menuModal.classList.add('active');
+                menuBtn.classList.add('nav-btn-active');
+            }
+        });
+
+
+        for( let i = 0; i < menuItems.length; i++ ) {
+            
+          
+            menuItems[i].addEventListener('click', function () {
+               
+                if (menuModal.classList.contains('active')) {
+                    menuModal.classList.remove('active');
+                    menuBtn.classList.remove('nav-btn-active');
+                } else {
+                    menuModal.classList.add('active');
+                    menuBtn.classList.add('nav-btn-active');
+                }
+            });
+        };
+
+
+
+
 }());
